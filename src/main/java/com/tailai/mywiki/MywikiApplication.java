7ff39704ab681916c23935982a3296421dff1aef
lvxueqing <2842726905@qq.com>
2024-01-19 14:13:00 +0800

    初始化

---
+++ src/main/java/com/tailai/mywiki/MywikiApplication.java
@@ -0,0 +1,13 @@
+package com.tailai.mywiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tailai/mywiki/MywikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailai.mywiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
