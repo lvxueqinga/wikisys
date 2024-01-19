fb289b06e01866bf8bbb24932a247d80a570f945
lvxueqing <2842726905@qq.com>
2024-01-19 21:26:40 +0800

    关闭规则

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-unused-components':0
+    'vue/no-unused-components': 'off'
   }
 }
