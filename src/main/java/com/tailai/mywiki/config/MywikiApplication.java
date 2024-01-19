f0e874b3af0097f969a854d9d07203fe3de43c27
lvxueqing <2842726905@qq.com>
2024-01-19 16:17:01 +0800

    修改启动文案

--- src/main/java/com/tailai/mywiki/config/MywikiApplication.java
+++ src/main/java/com/tailai/mywiki/config/MywikiApplication.java
@@ -1,17 +1,29 @@
 package com.tailai.mywiki.config;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.tailai")
 @MapperScan("com.tailai.mywiki.dao")
 public class MywikiApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(MywikiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(MywikiApplication.class, args);
+
+		SpringApplication app = new SpringApplication(MywikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+
+
+
+		LOG.info("启动成功！！");
+		LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
 	}
 
 }
