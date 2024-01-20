77383c02df656ee581f920a2e73387c67c371678
lvxueqing <2842726905@qq.com>
2024-01-20 19:33:20 +0800

    使用ref实现数据绑定

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,37 +1,13 @@
 <template>
   <div class="home">
-  <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
 
-    <template #renderItem="{ item }">
-      <a-list-item key="item.title">
-        <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component :is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
-        </template>
-        <template #extra>
-          <img
-                  width="272"
-                  alt="logo"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-          />
-        </template>
-        <a-list-item-meta :description="item.description">
-          <template #title>
-            <a :href="item.href">{{ item.title }}</a>
-          </template>
-          <template #avatar><a-avatar :src="item.avatar" /></template>
-        </a-list-item-meta>
-        {{ item.content }}
-      </a-list-item>
-    </template>
-  </a-list>
+        {{ebooks}}
+
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent , onMounted , ref} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from 'axios';
 
@@ -44,41 +20,22 @@ export default defineComponent({
   },
   setup() {
     console.log("mysetup");
-    axios.get("http://localhost:8888/book/search").then((response) => {
-      console.log(response);
+    const ebooks = ref();
+
+    onMounted(() => {
+      console.log("onMounted");
+      axios.get("http://localhost:8888/book/search").then((response) => {
+        console.log(response);
+        const data = response.data;
+        ebooks.value = data;
+      });
     });
 
 
-    const pagination = {
-      onChange: (page: number) => {
-        console.log(page);
-      },
-      pageSize: 3,
-    };
-    const actions: Record<string, string>[] = [
-      { type: 'StarOutlined', text: '156' },
-      { type: 'LikeOutlined', text: '156' },
-      { type: 'MessageOutlined', text: '2' },
-    ];
     return {
-      listData,
-      pagination,
-      actions,
+      ebooks,
     };
   },
 });
-const listData: Record<string, string>[] = [];
-
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    description:
-            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
 
 </script>
