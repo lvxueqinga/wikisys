6714421f941173e51259ba84662033df3ed769b5
lvxueqing <2842726905@qq.com>
2024-01-20 19:09:54 +0800

    vue集成axios， 解决跨域问题

---
+++ src/main/java/com/tailai/mywiki/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.tailai.mywiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+}
+
+
+// 解决跨域问题No 'Access-Control-Allow-Origin' header is present on the requested resource.
