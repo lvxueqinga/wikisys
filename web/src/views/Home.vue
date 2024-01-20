9967e827550c885c608cc38c80fb4e3adc9dc014
lvxueqing <2842726905@qq.com>
2024-01-20 19:47:40 +0800

    集成图标组件 npm install @ant-design/icons-vue --save

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,21 +1,67 @@
 <template>
-  <div class="home">
-
-        {{ebooks}}
-
-  </div>
+    <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+            <div>
+                <b>ant design vue</b>
+                footer part
+            </div>
+        </template>
+        <template #renderItem="{ item }">
+            <a-list-item key="item.title">
+                <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component :is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+                </template>
+                <template #extra>
+                    <img
+                            width="272"
+                            alt="logo"
+                            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                    />
+                </template>
+                <a-list-item-meta :description="item.description">
+                    <template #title>
+                        <a :href="item.href">{{ item.title }}</a>
+                    </template>
+                    <template #avatar><a-avatar :src="item.avatar" /></template>
+                </a-list-item-meta>
+                {{ item.content }}
+            </a-list-item>
+        </template>
+    </a-list>
 </template>
 
 <script lang="ts">
 import { defineComponent , onMounted , ref} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from 'axios';
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+    listData.push({
+        href: 'https://www.antdv.com/',
+        title: `ant design vue part ${i}`,
+        avatar: 'https://joeschmoe.io/api/v1/random',
+        description:
+            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+}
+
+
 
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+      StarOutlined,
+      LikeOutlined,
+      MessageOutlined,
 
   },
   setup() {
@@ -31,11 +77,31 @@ export default defineComponent({
       });
     });
 
+      const pagination = {
+          onChange: (page: number) => {
+              console.log(page);
+          },
+          pageSize: 3,
+      };
+      const actions: Record<string, string>[] = [
+          { type: 'StarOutlined', text: '156' },
+          { type: 'LikeOutlined', text: '156' },
+          { type: 'MessageOutlined', text: '2' },
+      ];
+
+
 
     return {
       ebooks,
+        listData,
+        pagination,
+        actions,
     };
   },
 });
+
+
+
+
 
 </script>
