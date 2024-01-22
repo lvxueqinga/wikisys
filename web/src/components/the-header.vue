2739f57c9a8e23e771a8f1658d3da21868e3e1b7
lvxueqing <2842726905@qq.com>
2024-01-22 11:42:11 +0800

    增加电子书管理页面（页面、路由、菜单） ； 增加导航链接跳转

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,9 +7,15 @@
                 mode="horizontal"
                 :style="{ lineHeight: '64px' }"
         ><!--                v-model:selectedKeys="selectedKeys1"-->
-            <a-menu-item key="1">首页</a-menu-item>
-            <a-menu-item key="2">电子书管理</a-menu-item>
-            <a-menu-item key="3">关于我们</a-menu-item>
+            <a-menu-item key="/">
+                <router-link to="/">首页</router-link>
+            </a-menu-item>
+            <a-menu-item key="/admin/ebook">
+                <router-link to="/admin/ebook">电子书管理</router-link>
+            </a-menu-item>
+            <a-menu-item key="/about">
+                <router-link to="/about">关于我们</router-link>
+            </a-menu-item>
         </a-menu>
     </a-layout-header>
 </template>
