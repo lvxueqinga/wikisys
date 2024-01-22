6b1dbe1cf6c725550b1e5a0da9cbe99220f5d26a
lvxueqing <2842726905@qq.com>
2024-01-22 13:07:37 +0800

    使用PageHelper实现后端分页

--- src/main/java/com/tailai/mywiki/service/Impl/BookServiceImpl.java
+++ src/main/java/com/tailai/mywiki/service/Impl/BookServiceImpl.java
@@ -1,8 +1,13 @@
 package com.tailai.mywiki.service.Impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.tailai.mywiki.config.MywikiApplication;
 import com.tailai.mywiki.dao.BookMapper;
 import com.tailai.mywiki.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +22,18 @@ import java.util.List;
  */
 @Service
 public class BookServiceImpl implements BookService {
+    private static final Logger LOG = LoggerFactory.getLogger(MywikiApplication.class);
+
     @Autowired
     private BookMapper bookMapper;
 
     @Override
     public List<JSONObject> searchBook() {
-        return bookMapper.searchBook();
+        PageHelper.startPage(1,2);
+        List<JSONObject> ans = bookMapper.searchBook();
+        PageInfo<JSONObject> pageInfo = new PageInfo<>(ans);
+        LOG.info("总行数 {}",pageInfo.getTotal());
+        LOG.info("总页数 {}",pageInfo.getPages());
+        return ans;
     }
 }
