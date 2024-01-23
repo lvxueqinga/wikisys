84d5891fbc7c3af3c620cbc56b57312c1e17bd0a
lvxueqing <2842726905@qq.com>
2024-01-23 13:43:17 +0800

    解决前端调用判断notnull未生效问题

--- src/main/java/com/tailai/mywiki/model/Book.java
+++ src/main/java/com/tailai/mywiki/model/Book.java
@@ -2,6 +2,7 @@ package com.tailai.mywiki.model;
 import lombok.Data;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -17,6 +18,7 @@ public class Book {
     private Integer id;
     @NotNull(message = "名称不能为空")
     @Size(max=50 , message = "名称不能超过50字")
+    @NotEmpty(message = "名称不能为空")
     private String name;
     private String category;
     private String pic;
